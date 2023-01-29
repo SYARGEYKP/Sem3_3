b8fb389a9378ec21917dd673d3f3e453169aa7e4
SYARGEY <lss1895@mail.ru>
2023-01-29 19:55:06 +0300

    Семинар  задание 3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage} > ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int num = InputInt("Введите число");
+int i = 1;
+while (i <= num)
+{
+    System.Console.Write($" {i * i * i}");
+    i++;
+}
+
